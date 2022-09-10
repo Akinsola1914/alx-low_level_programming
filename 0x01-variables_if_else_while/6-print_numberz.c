6795981b861c1b8a872001093e16ed3417dea276
akinsola <akinmulegunakinsola123@gmail.com>
2022-09-10 00:25:34 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int A;
+
+	for (A = '0'; A <= '9'; A++)
+		putchar(A);
+	putchar('\n');
+	return (0);
+}
