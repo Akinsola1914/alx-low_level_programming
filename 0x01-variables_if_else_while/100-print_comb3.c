522a04f9abf42a0060495fdf3c0ede99b2568cea
akinsola <akinmulegunakinsola123@gmail.com>
2022-09-10 00:54:29 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int T;
+
+	for (T = 0; T < 100; T++)
+	{
+		putchar((T / 10) + '0');
+		putchar((T % 10) + '0');
+	if (T != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
